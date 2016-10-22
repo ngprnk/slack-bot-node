fb8af953ed97188db7f68a87f79996a64169903a
Nischal Gautam <nischal1024@gmail.com>
2016-10-22 22:53:03 +0545

    separed facebook tokens to config fbConfig file

---
+++ app/config/fbConfig.js
@@ -0,0 +1,7 @@
+var config = {
+  access_token: "EAAE5XHGdEBMBAGJmVL8oGCx78dBaHqjXX5WneMW4erMRKXXTkojM5Oh4KcyMijmoJNNLl2ZChnFl1ExRH34ggLK0M5fiJX0yDkjSdn5cxlUGGLRA9JkN6qnHIhoyzfg5f2mOY9BZBouA91cWZBQugVvo55cBIgZD",
+  query: "Nepal",
+  type: "event"
+};
+
+module.exports = config;
--- app/index.js
+++ app/index.js
@@ -6,6 +6,7 @@ var mongoose = require('mongoose');
 var dbPath = require('./db-config');
 var Event = require('./models/event.js');
 var botConfig = require('./config/botConfig');
+var fbConfig = require('./config/fbConfig');
 var Slackbok = require('slackbots');
 
 var bot = new Slackbok({
@@ -20,7 +21,7 @@ bot.on('start', function() {
     };
 
     // define channel, where bot exist. You can adjust it there https://my.slack.com/services
-    bot.postMessageToChannel('bot-testing', 'https://www.facebook.com/events/1131952436816496', params);
+    bot.postMessageToChannel('bot-testing', 'New event comming up', params);
 
 
 });
@@ -34,8 +35,8 @@ db.once('open',function(){console.log("Mongodb Database connected");});
 
 
 var url = "https://graph.facebook.com/";
-config = {access_token: "EAAE5XHGdEBMBAGJmVL8oGCx78dBaHqjXX5WneMW4erMRKXXTkojM5Oh4KcyMijmoJNNLl2ZChnFl1ExRH34ggLK0M5fiJX0yDkjSdn5cxlUGGLRA9JkN6qnHIhoyzfg5f2mOY9BZBouA91cWZBQugVvo55cBIgZD"};
-parameters = {q: "Nepal", type: "event"};
+config = {access_token: fbConfig.access_token};
+parameters = {q: fbConfig.query, type: fbConfig.type};
 
 
 
