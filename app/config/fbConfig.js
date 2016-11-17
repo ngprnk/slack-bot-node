96d4d71b3c4200848694ca59d9198b27b7325033
Nischal Gautam <nischal1024@gmail.com>
2016-11-17 22:55:55 +0545

    Update fbConfig.js

--- app/config/fbConfig.js
+++ app/config/fbConfig.js
@@ -1,5 +1,5 @@
 var config = {
-  access_token: "EAAE5XHGdEBMBAGJmVL8oGCx78dBaHqjXX5WneMW4erMRKXXTkojM5Oh4KcyMijmoJNNLl2ZChnFl1ExRH34ggLK0M5fiJX0yDkjSdn5cxlUGGLRA9JkN6qnHIhoyzfg5f2mOY9BZBouA91cWZBQugVvo55cBIgZD",
+  access_token: "token changed",
   query: "Nepal",
   type: "event",
   url: "https://graph.facebook.com/"
